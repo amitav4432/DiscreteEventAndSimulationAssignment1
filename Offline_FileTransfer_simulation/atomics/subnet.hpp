d697d867a1f50def3f2b2127d25572466fab5b6f
Amitav Shaw <amitav.shaw@gmail.com>
2019-10-18 22:22:20 -0400

    DEVS Model Implementation in Cadmium
    Offline file transfer simulation using Cadmium. 
    For Assignment 1 of Methodologies of Discrete Event and Simulation

---
+++ Offline_FileTransfer_simulation/atomics/subnet.hpp
@@ -0,0 +1,91 @@
+/**
+* Cristina Ruiz Martin
+* ARSLab - Carleton University
+* Used in assignment and modified by Amitav/Arshpreet
+* Subnet:
+* Cadmium implementation of CD++ Subnet atomic model
+*/
+
+#ifndef _SUBNET_HPP__
+#define _SUBNET_HPP__
+
+#include <cadmium/modeling/ports.hpp>
+#include <cadmium/modeling/message_bag.hpp>
+
+#include <limits>
+#include <assert.h>
+#include <string>
+#include <random>
+
+#include "../data_structures/message.hpp"
+
+using namespace cadmium;
+using namespace std;
+
+//Port definition
+struct Subnet_defs{
+    struct out : public out_port<Message_t> {};
+    struct in : public in_port<Message_t> {};
+};
+
+template<typename TIME> class Subnet{
+    public:
+    // ports definition
+    using input_ports=tuple<typename Subnet_defs::in>;
+    using output_ports=tuple<typename Subnet_defs::out>;
+    // state definition
+    struct state_type{
+        bool transmitting;
+        Message_t packet;
+        int index;
+    }; 
+    state_type state;    
+    // default constructor
+    Subnet() {
+        state.transmitting = false;
+        state.index        = 0;
+    }     
+    // internal transition
+    void internal_transition() {
+        state.transmitting = false;  
+    }
+    // external transition
+    void external_transition(TIME e, typename make_message_bags<input_ports>::type mbs) { 
+        vector<Message_t> bag_port_in;
+        bag_port_in = get_messages<typename Subnet_defs::in>(mbs);
+        if(bag_port_in.size()>1) assert(false && "One message at a time");                
+        state.index ++;               
+        state.packet = bag_port_in[0];
+        state.transmitting = true;                               
+    }
+    // confluence transition
+    void confluence_transition(TIME e, typename make_message_bags<input_ports>::type mbs) {
+        internal_transition();
+        external_transition(TIME(), move(mbs));
+    }
+    // output function
+    typename make_message_bags<output_ports>::type output() const {
+        typename make_message_bags<output_ports>::type bags;
+        vector<Message_t> bag_port_out;   
+	//bag_port_out.push_back(state.packet);
+        //get_messages<typename Subnet_defs::out>(bags) = bag_port_out;          
+        get_messages<typename Subnet_defs::out>(bags).push_back(state.packet); 
+        return bags;
+    }
+    // time_advance function
+    TIME time_advance() const {
+        TIME next_internal;
+        if (state.transmitting) {            
+            next_internal = TIME("00:00:03:000");
+        }else {
+            next_internal = numeric_limits<TIME>::infinity();
+        }    
+        return next_internal;
+    }
+
+    friend ostringstream& operator<<(ostringstream& os, const typename Subnet<TIME>::state_type& i) {
+        os << "index: " << i.index << " & transmitting: " << i.packet; 
+        return os;
+    }
+};    
+#endif // _SUBNET_HPP__
